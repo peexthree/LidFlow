27db499f8d35719dd1fdd937238cb416180f19ff
peexthree <linedrop@yandex.ru>
2025-10-10 14:13:01 +0300

    Fix: Corrected case sensitivity for Galaxy.tsx component

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // было: experimental: { typedRoutes: true }
-  typedRoutes: true,
-};
-
-export default nextConfig;
